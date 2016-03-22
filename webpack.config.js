67442cd844d22ca45175f7a21a303960d5e19662
sunhengzhe <sunhengzhe@renrenche.com>
2016-03-22 14:45:28 +0800

    use react-hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 var path = require('path');
+var webpack = require('webpack');
 
 var localPath = __dirname.replace(/\\/g, '\/');
 var remotePath = 'http://sunhengzhe.sinaapp.com/react-iphone';
 module.exports = {
-  entry: "./src/app.js",
+  entry: [
+    "webpack-dev-server/client?http://0.0.0.0:3000",
+    "webpack/hot/only-dev-server",
+    "./src/app.js",
+  ],
   output: {
-    path: __dirname,
-    filename: "build/js/bundle.js",
-    publicPath: remotePath
+    path: path.join(__dirname, 'build'),
+    filename: "js/bundle.js",
+    publicPath: "build/"
   },
   module: {
     loaders: [
@@ -17,16 +22,19 @@ module.exports = {
       },
       {
         test: /\.(png|jpg)$/,
-        loader: 'url-loader?limit=8192,name=/build/images/[hash].[ext]',
+        loader: 'url-loader?limit=8192,name=images/[hash].[ext]',
       },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel',
+        loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015'],
         query: {
           presets: ['react', 'es2015']
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 }
